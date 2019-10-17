5e61181ce1c13f15dab4e501e9d1d51ca2669188
Damian Cichosz <damian.cichosz97@gmail.com>
2019-10-17 21:18:55 +0200

    added helper to make shortcut and move app to autostart folder

---
+++ HasselSoft/TrollHelpers/Shortcut.cs
@@ -0,0 +1,69 @@
+﻿/*
+ * EXAMPLE USAGE
+ *
+ * var lnkFileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "Notepad.lnk");
+
+			Shortcut.Create(lnkFileName,
+				System.IO.Path.Combine(Shortcut.AppLocation, Shortcut.AppName),
+				null, null, "Open Notepad", "Ctrl+Shift+N", null);
+ */
+
+
+using System;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace HasselSoft.TrollHelpers
+{
+	public static class Shortcut
+	{
+		public static string AppLocation => AppDomain.CurrentDomain.BaseDirectory;
+		public static string AppName => $"{AppDomain.CurrentDomain.FriendlyName}.exe";
+
+
+		private static Type m_type = Type.GetTypeFromProgID("WScript.Shell");
+		private static object m_shell = Activator.CreateInstance(m_type);
+
+		[ComImport, TypeLibType((short)0x1040), Guid("F935DC23-1CF0-11D0-ADB9-00C04FD58A0B")]
+		private interface IWshShortcut
+		{
+			[DispId(0)]
+			string FullName { [return: MarshalAs(UnmanagedType.BStr)] [DispId(0)] get; }
+			[DispId(0x3e8)]
+			string Arguments { [return: MarshalAs(UnmanagedType.BStr)] [DispId(0x3e8)] get; [param: In, MarshalAs(UnmanagedType.BStr)] [DispId(0x3e8)] set; }
+			[DispId(0x3e9)]
+			string Description { [return: MarshalAs(UnmanagedType.BStr)] [DispId(0x3e9)] get; [param: In, MarshalAs(UnmanagedType.BStr)] [DispId(0x3e9)] set; }
+			[DispId(0x3ea)]
+			string Hotkey { [return: MarshalAs(UnmanagedType.BStr)] [DispId(0x3ea)] get; [param: In, MarshalAs(UnmanagedType.BStr)] [DispId(0x3ea)] set; }
+			[DispId(0x3eb)]
+			string IconLocation { [return: MarshalAs(UnmanagedType.BStr)] [DispId(0x3eb)] get; [param: In, MarshalAs(UnmanagedType.BStr)] [DispId(0x3eb)] set; }
+			[DispId(0x3ec)]
+			string RelativePath { [param: In, MarshalAs(UnmanagedType.BStr)] [DispId(0x3ec)] set; }
+			[DispId(0x3ed)]
+			string TargetPath { [return: MarshalAs(UnmanagedType.BStr)] [DispId(0x3ed)] get; [param: In, MarshalAs(UnmanagedType.BStr)] [DispId(0x3ed)] set; }
+			[DispId(0x3ee)]
+			int WindowStyle { [DispId(0x3ee)] get; [param: In] [DispId(0x3ee)] set; }
+			[DispId(0x3ef)]
+			string WorkingDirectory { [return: MarshalAs(UnmanagedType.BStr)] [DispId(0x3ef)] get; [param: In, MarshalAs(UnmanagedType.BStr)] [DispId(0x3ef)] set; }
+			[TypeLibFunc((short)0x40), DispId(0x7d0)]
+			void Load([In, MarshalAs(UnmanagedType.BStr)] string PathLink);
+			[DispId(0x7d1)]
+			void Save();
+		}
+
+		public static void Create(string fileName, string targetPath, string arguments, string workingDirectory, string description, string hotkey, string iconPath)
+		{
+			IWshShortcut shortcut = (IWshShortcut)m_type.InvokeMember("CreateShortcut", System.Reflection.BindingFlags.InvokeMethod, null, m_shell, new object[] { fileName });
+			shortcut.Description = description;
+			shortcut.Hotkey = hotkey;
+			shortcut.TargetPath = targetPath;
+			shortcut.WorkingDirectory = workingDirectory;
+			shortcut.Arguments = arguments;
+			if (!string.IsNullOrEmpty(iconPath))
+				shortcut.IconLocation = iconPath;
+			shortcut.Save();
+		}
+
+		public static void PermamentAutoStart() => File.Copy(Path.Combine(Shortcut.AppLocation, Shortcut.AppName), Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Startup), Path.GetFileName(Path.Combine(AppLocation, AppName))));
+	}
+}
