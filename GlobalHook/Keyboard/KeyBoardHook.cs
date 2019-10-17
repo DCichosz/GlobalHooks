ebc390be6e675951b1dc7f5a85a6eda1ccb52fb4
Damian Cichosz <damian.cichosz97@gmail.com>
2019-10-17 21:18:30 +0200

    added example usage of keyboard hook

--- GlobalHook/Keyboard/KeyBoardHook.cs
+++ GlobalHook/Keyboard/KeyBoardHook.cs
@@ -2,16 +2,20 @@
  Example USAGE !!
 
 
-  static void Main(string[] args)
-        {
-            Keyboard.SetHook();
-
-            while ((!NativeMethods.GetMessage(out NativeMethods.MSG msg, IntPtr.Zero, 0, 0)))
+  Mouse.OnLeftButtonDown += (sender, e) =>
             {
-                NativeMethods.TranslateMessage(ref msg);
-                NativeMethods.DispatchMessage(ref msg);
-            }
-        }
+                Console.WriteLine(JsonSerializer.Serialize(e));
+            };
+
+            Keyboard.OnKeyPress += (sender, e) =>
+            {
+                Console.WriteLine(e.Key);
+            };
+
+            Console.WriteLine(Keyboard.SetHook()  != default ? "Hooked keyboard" : "Couldn't hook keyboard");
+            Console.WriteLine(Mouse.SetHook()  != default ? "Hooked mouse" : "Couldn't hook mouse");
+
+            NativeMethods.StartListening();
  */
 
 
